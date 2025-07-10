d6f6eca6c8ca2e7976b71abbe3ba5d07c8dc1ad3
iamjiamingliu <57842784+iamjiamingliu@users.noreply.github.com>
2025-07-10 15:06:27 -0700

    reorganize files into src

---
+++ src/searchbar.js
@@ -0,0 +1,4 @@
+const native_searchbar = document.getElementsByClassName("AppHeader-search")[0]
+const searchgit_searchbar = document.createElement("input");
+searchgit_searchbar.placeholder = "Ligma ball"
+native_searchbar.replaceWith(searchgit_searchbar)
