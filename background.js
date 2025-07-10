34df4c8b1598e8367f5dd98c13fcbdba72f8f5f9
iamjiamingliu <57842784+iamjiamingliu@users.noreply.github.com>
2025-07-10 12:22:20 -0700

    it works!

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setBadgeText({
-        text: "OFF",
+        text: 'OFF'
     });
 });
 
@@ -19,18 +19,19 @@ chrome.action.onClicked.addListener(async (tab) => {
             tabId: tab.id,
             text: nextState,
         });
-    }
-    if (nextState === "ON") {
-        // Insert the CSS file when the user turns the extension on
-        await chrome.scripting.insertCSS({
-            files: ["focus-mode.css"],
-            target: { tabId: tab.id },
-        });
-    } else if (nextState === "OFF") {
-        // Remove the CSS file when the user turns the extension off
-        await chrome.scripting.removeCSS({
-            files: ["focus-mode.css"],
-            target: { tabId: tab.id },
-        });
+
+        if (nextState === 'ON') {
+            // Insert the CSS file when the user turns the extension on
+            await chrome.scripting.insertCSS({
+                files: ['focus-mode.css'],
+                target: { tabId: tab.id }
+            });
+        } else if (nextState === 'OFF') {
+            // Remove the CSS file when the user turns the extension off
+            await chrome.scripting.removeCSS({
+                files: ['focus-mode.css'],
+                target: { tabId: tab.id }
+            });
+        }
     }
 });
