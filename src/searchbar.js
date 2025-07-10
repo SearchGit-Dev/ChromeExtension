8d30a709b0ebdcde64795f975856ff2d05c35596
iamjiamingliu <57842784+iamjiamingliu@users.noreply.github.com>
2025-07-10 15:21:28 -0700

    got autocomplete.js to work!!!

--- src/searchbar.js
+++ src/searchbar.js
@@ -1,4 +1,15 @@
 const native_searchbar = document.getElementsByClassName("AppHeader-search")[0]
 const searchgit_searchbar = document.createElement("input");
-searchgit_searchbar.placeholder = "Ligma ball"
+searchgit_searchbar.id = "searchgit-searchbar";
+searchgit_searchbar.placeholder = "Search Github"
 native_searchbar.replaceWith(searchgit_searchbar)
+new autoComplete({
+    selector: "#searchgit-searchbar",
+    placeHolder: "Search for Food...",
+    data: {
+        src: ["Sauce - Thousand Island", "Wild Boar - Tenderloin", "Goat - Whole Cut"]
+    },
+    resultItem: {
+        highlight: true,
+    }
+});
