58dc93e25e3631cd2ee4bc332dc024e6b1bddc51
iamjiamingliu <57842784+iamjiamingliu@users.noreply.github.com>
2025-07-10 15:27:08 -0700

    detects color mode

--- src/searchbar.js
+++ src/searchbar.js
@@ -1,11 +1,13 @@
+const color_mode = document.documentElement.getAttribute("data-color-mode");
+
 const native_searchbar = document.getElementsByClassName("AppHeader-search")[0]
-const searchgit_searchbar = document.createElement("input");
-searchgit_searchbar.id = "searchgit-searchbar";
-searchgit_searchbar.placeholder = "Search Github"
+const searchgit_searchbar = document.createElement("input")
+searchgit_searchbar.id = "searchgit-searchbar"
 native_searchbar.replaceWith(searchgit_searchbar)
+
 new autoComplete({
     selector: "#searchgit-searchbar",
-    placeHolder: "Search for Food...",
+    placeHolder: "Search for Food..." + color_mode,
     data: {
         src: ["Sauce - Thousand Island", "Wild Boar - Tenderloin", "Goat - Whole Cut"]
     },
