0f99c32a38d34982ab3119c80eef68c71f4be28e
iamjiamingliu <57842784+iamjiamingliu@users.noreply.github.com>
2025-07-10 15:42:56 -0700

    adjusts spacing again

--- src/searchbar.js
+++ src/searchbar.js
@@ -1,9 +1,16 @@
 const color_mode = document.documentElement.getAttribute("data-color-mode");
 
 const native_searchbar = document.getElementsByClassName("AppHeader-search")[0]
+
 const searchgit_searchbar = document.createElement("input")
 searchgit_searchbar.id = "searchgit-searchbar"
-native_searchbar.replaceWith(searchgit_searchbar)
+
+const searchgit_searchbar_div = document.createElement("div")
+searchgit_searchbar_div.style.paddingRight = "28px"
+searchgit_searchbar_div.style.marginTop = "-4px"
+searchgit_searchbar_div.appendChild(searchgit_searchbar)
+
+native_searchbar.replaceWith(searchgit_searchbar_div)
 
 new autoComplete({
     selector: "#searchgit-searchbar",
@@ -15,7 +22,3 @@ new autoComplete({
         highlight: true,
     }
 });
-
-searchgit_searchbar.style.marginRight = "28px"
-searchgit_searchbar.style.marginTop = "-4px"
-
